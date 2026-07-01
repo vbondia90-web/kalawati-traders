7902064bce1535c712b9afeae780817e65ed849d
Anant Bondia <vbondia90@gmail.com>
2026-07-01 22:36:33 +0530

    Fix component folder imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 import LoadingScreen from "./components/LoadingScreen";
+
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import FloatingButtons from "./components/layout/FloatingButtons";
+
 import Hero from "./components/home/Hero";
 import Stats from "./components/home/Stats";
 import About from "./components/home/About";
@@ -12,14 +16,11 @@ import Gallery from "./components/home/Gallery";
 import GoogleReviews from "./components/home/GoogleReviews";
 import Contact from "./components/home/Contact";
 import QuoteForm from "./components/home/QuoteForm";
-import Footer from "./components/layout/Footer";
-import FloatingButtons from "./components/layout/FloatingButtons";
 
 export default function App() {
   return (
     <>
       <LoadingScreen />
-
       <Navbar />
 
       <main>
@@ -36,7 +37,6 @@ export default function App() {
       </main>
 
       <Footer />
-
       <FloatingButtons />
     </>
   );
